386fcac96daf70e300af6b7c121a49bbe5cb09e3
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-08-13 12:35:08 +0300

    Header nav + CSS

---
+++ header.php
@@ -0,0 +1,5 @@
+<nav>
+        <a href="./matches.php">Топ матчей</a>
+        <a href="./stardomparsing.php">Топ рестлеров</a>
+        <a href="./adding.php">Добавить матч</a>
+</nav>
--- stardomparsing.php
+++ stardomparsing.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/styles.css">
     <title>Top Wrestlers</title>
 </head>
 <body>
     <?php
+    require_once 'header.php';
     function multiexplode ($delimiters,$string) {
 
         $ready = str_replace($delimiters, $delimiters[0], $string);
