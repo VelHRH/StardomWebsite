e49cffb8c353d3f98dad242c3be5f1f858c62d88
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-08-13 20:41:07 +0300

    Matches list became prettier

--- matches.php
+++ matches.php
@@ -7,6 +7,28 @@
     <link rel="stylesheet" href="./styles/styles.css">
     <title>Top Matches</title>
 </head>
+<style>
+    table{
+        display: flex;
+        justify-content: center;
+        border-collapse:separate; 
+        border-spacing: 0.5em 0.5em;
+    }
+table th, td {
+    padding:.25em .5em;
+    text-align:left;
+    border-radius: 10px;
+    margin: 10px;
+  }
+  td {
+    background-color:#eee; 
+       
+  }
+  th {
+    background-color:#009;
+    color:#fff;
+  }
+</style>
 <body>
     <?php
     require_once 'header.php';
@@ -14,22 +36,72 @@
     $wrestlers = array();
     $mysql = new mysqli('localhost', 'root', '', 'betdb');
     $result = $mysql->query("SELECT * FROM `stardom` order by `Рейтинг` desc");
-    $i=0;
+    ?>
+    <table>
+    <tr>
+        <th>Match</th>
+        <th>Rating</th>
+    </tr>
+    <?php
     while ($match = $result->fetch_assoc()){
-        $i++;
-        $j=0;
-        foreach ($match as $k => $v){
-            if ($j==0){echo "$i. $v | ";}
-            elseif ($j==3){
-                echo "$v";
-            }
-            else {
-                echo "$v | ";
-            }
-            $j++;
+        $i = $match['Матч'];
+        $j = $match['Рейтинг'];
+        if ($j==5){
+        ?> 
+        <tr> 
+            <td><a href="#"><?php echo $i; ?></a></td>
+            <td style="background-color: rgb(0, 81, 0); color: white;"><?php echo $j; ?></td>
+        </tr>
+        
+        <?php
         }
-        ?> <br> <?php
+        if ($j==4.75){
+            ?> 
+            <tr> 
+                <td><a href="#"><?php echo $i; ?></a></td>
+                <td style="background-color: rgb(0, 111, 0); color: white;"><?php echo $j; ?></td>
+            </tr>
+            
+            <?php
+        }
+        if ($j==4.5){
+            ?> 
+            <tr> 
+                <td><a href="#"><?php echo $i; ?></a></td>
+                <td style="background-color: rgb(0, 141, 0); color: white;"><?php echo $j; ?></td>
+            </tr>
+            
+            <?php
+        }
+        if ($j==4.25){
+            ?> 
+            <tr> 
+                <td><a href="#"><?php echo $i; ?></a></td>
+                <td style="background-color: rgb(0, 171, 0); color: white;"><?php echo $j; ?></td>
+            </tr>
+            
+            <?php
+        }
+        if ($j==4){
+            ?> 
+            <tr> 
+                <td><a href="#"><?php echo $i; ?></a></td>
+                <td style="background-color: rgb(0, 201, 0); color: white;"><?php echo $j; ?></td>
+            </tr>
+            
+            <?php
+        }
+        if ($j==3.75){
+            ?> 
+            <tr> 
+                <td><a href="#"><?php echo $i; ?></a></td>
+                <td style="background-color: rgb(0, 231, 0); color: white;"><?php echo $j; ?></td>
+            </tr>
+            
+            <?php
+        }
     }
     ?>
+    </table>
 </body>
 </html>
