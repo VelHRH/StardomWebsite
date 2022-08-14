8a7f7b645ead14138e56a729fc3fd75aafdf021b
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-08-14 11:44:17 +0300

    Created slide out bar

--- header.php
+++ header.php
@@ -1,5 +1,12 @@
+<link rel="stylesheet" href="./styles/styles.css">
+<script src="menubtn.js"></script>
 <nav>
-        <a href="./matches.php">Топ матчей</a>
+        <a onclick="show()">Топ матчей</a>
         <a href="./stardomparsing.php">Топ рестлеров</a>
         <a href="./adding.php">Добавить матч</a>
 </nav>
+
+<div id="slidebar">
+        <a href="./matches.php"><img class="imgmenu" src="https://www.pwtorch.com/site/wp-content/uploads/post/2019/06/world-wonder-ring-stardom.png" alt="Stardom"></a>
+        <a href="./matches.php"><img class="imgmenu" src="https://www.pwponderings.com/wp-content/uploads/2022/03/PsUnZjWA1e9fCOF7rS1T.jpg" alt="AJW"></a>
+</div>
