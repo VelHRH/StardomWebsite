26b531d8bd51b6557306ca41cd1f0d503074f081
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2022-08-20 13:05:30 +0300

    Added check for invalid input

--- addhandlegp.php
+++ addhandlegp.php
@@ -1,9 +1,9 @@
 <?php
 
-    $match = filter_var(trim($_POST['mname']), FILTER_SANITIZE_STRING);
-    $show = filter_var(trim($_POST['show']), FILTER_SANITIZE_STRING);
-    $year = filter_var(trim($_POST['year']), FILTER_SANITIZE_STRING);
-    $rating = filter_var(trim($_POST['rating']), FILTER_SANITIZE_STRING);
+    $MA = filter_var(trim($_POST['mname']), FILTER_SANITIZE_STRING);
+    $SH = filter_var(trim($_POST['show']), FILTER_SANITIZE_STRING);
+    $YE = filter_var(trim($_POST['year']), FILTER_SANITIZE_STRING);
+    $RA = filter_var(trim($_POST['rating']), FILTER_SANITIZE_STRING);
     
 
 
@@ -11,19 +11,73 @@
 
 
 
+    $mysql = new mysqli('localhost', 'root', '', 'stardom');
+    $res = $mysql->query("SELECT `Рестлер1` FROM `5stargp` ORDER BY `Рейтинг` desc");
+            $wrsetlers = array();
+            while ($match = $res->fetch_assoc()){
+                $i = $match['Рестлер1'];
+                $kontrol = 0;
+                foreach ($wrsetlers as $k => $v){
+                    if ($v==$i){
+                        $kontrol++;
+                        break;
+                    }
+                }
+                if($kontrol==0){
+                    $wrsetlers[] = $i;
+                }
+            }
+            $res = $mysql->query("SELECT `Рестлер2` FROM `5stargp` ORDER BY `Рейтинг` desc");
+            while ($match = $res->fetch_assoc()){
+                $i = $match['Рестлер2'];
+                $kontrol = 0;
+                foreach ($wrsetlers as $k => $v){
+                    if ($v==$i){
+                        $kontrol++;
+                        break;
+                    }
+                }
+                if($kontrol==0){
+                    $wrsetlers[] = $i;
+                }
+            }
+            $w = array();
+            foreach ($wrsetlers as $k => $v){
+                $res = $mysql->query("SELECT AVG(`Рейтинг`) AS `avg` FROM `5stargp` WHERE `Рестлер1`='$v' or `Рестлер2`='$v'");
+                $m = $res->fetch_assoc();
+                $okr = $m['avg'];
+                $okr = round($okr, 2);
+                $w[$v]=$okr;
+            }
 
-    
+            $kont=0;
+            foreach ($w as $k => $v){
+                if ($MA==$k){
+                    $kont++;
+                    break;
+                }
+            }
+            $kont1=0;
+            foreach ($w as $k => $v){
+                if ($SH==$k){
+                    $kont1++;
+                    break;
+                }
+            }
+            if ($kont==0) {echo "Неправильный ввод 'Рестлер 1'"; echo "<br>";
+            }
+            elseif ($kont1==0) {echo "Неправильный ввод 'Рестлер 2'"; echo "<br>";
+            }
 
 
 
 
 
-    if (($match=="") || ($show=="") || ($year=="") || ($rating=="None")){
-        echo "Uncorrect input!";
+    elseif (($MA=="") || ($SH=="") || ($YE=="") || ($RA=="None")){
+        echo "Пустые поля!"; echo "<br>";
     }
     else{
-        $mysql = new mysqli('localhost', 'root', '', 'stardom');
-        $result = $mysql->query("INSERT INTO `5stargp`(`Рестлер1`, `Рестлер2`, `Год`, `Рейтинг`) VALUES ('$match','$show','$year','$rating')");
+        $result = $mysql->query("INSERT INTO `5stargp`(`Рестлер1`, `Рестлер2`, `Год`, `Рейтинг`) VALUES ('$MA','$SH','$YE','$RA')");
         $mysql->close();
         header('Location: gp2022.php');
     }
